290f9c9e24560b2c4504f0c0e7e0b69cab18e900
thomas <thomas@massively.ai>
2017-12-04 14:46:48 -0500

    lesson 1

--- lesson1.py
+++ lesson1.py
@@ -1,4 +1,21 @@
-import nltk
-
+from nltk.tokenize import sent_tokenize,word_tokenize
 
 #nltk.download()
+
+#tokenizing - two types grouping by words and groups by sentences
+#lexicon - word and what it means
+#corpora - body of text .. 
+
+#investor-speak.... regular english-speak
+
+#investor speak 'bull' = positive in market
+#english-speak 'bull' = animal
+
+example_text="hello Mr. Thomas,how are you doing.We are very pleased to see you. The service was bad but the food was good."
+
+
+print(sent_tokenize(example_text))
+#print(word_tokenize(example_text))
+
+for word in word_tokenize(example_text):
+    print(word)
